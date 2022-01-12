d464bb603b2eb2de388a3970fcf2d34d07f098e9
luizfilipebp <luizfilipe098@gmail.com>
2022-01-12 14:42:44 -0300

    Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Navbar from './components/Navbar';
 import logo from './logo.svg';
 
 function App() {
   return (
-    <h1>Página principal</h1>
+    <Navbar/>
   );
 }
 
